e5365b378d395629e061335ec7d0e6de3f16c389
deodato_bastos <deodatobasto@gmail.com>
2022-08-27 02:06:16 -0300

    fix: fix variables names and change out of Pair

--- src/containers/Pair.h
+++ src/containers/Pair.h
@@ -4,17 +4,18 @@
 
 #include <iostream>
 
-template<class T, class B>
+template <class T, class B>
 class Pair {
 private:
 	T first;
 	B second;
+
 public:
 	Pair(T First, B Second) : first(First), second(Second) {}
 	~Pair() {}
 
-	friend std::ostream &operator<< (std::ostream& os, const Pair<T, B>& p) {
-		os << "first: " << p._first << "second: " << p._second << std::endl;
+	friend std::ostream &operator<<(std::ostream &os, const Pair<T, B> &p) {
+		os << "(" << p.first << ", " << p.second << ")";
 		return os;
 	}
 };
