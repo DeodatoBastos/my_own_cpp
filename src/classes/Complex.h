e48fe79c59d98e04eb6f0d41c6773139da377cd7
deodato_bastos <deodatobasto@gmail.com>
2022-08-27 02:38:54 -0300

    feat: create others constructors and assign operator for Complex

--- src/classes/Complex.h
+++ src/classes/Complex.h
@@ -2,14 +2,26 @@
 #define COMPLEX_H
 
 #include <iostream>
+#include <cmath>
 
 template<class C>
 class Complex {
 public:
 	C real, img;
 
-	Complex<C>(C r, C i) : real(r), img(i) {}
-	~Complex<C>() {}
+	Complex<C> () : real(0), img(0) {}
+	Complex<C> (C r, C i) : real(r), img(i) {}
+	Complex<C> (C theta) : real(cos(theta)), img(sin(theta)) {}
+	Complex<C> (C multiplier, const Complex<C>& w): real(multiplier * w.real), img(multiplier * w.img) {}
+
+	Complex<C>& operator= (const Complex<C>& w) {
+		this-> real = w.real;
+		this->img = w.img;
+
+		return *this;
+	}
+
+	~Complex<C> () {}
 
 	friend std::ostream &operator<< (std::ostream& os, const Complex<C>& z) {
 		os << z.real << " + " << z.img << "i";
